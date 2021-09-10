6bdc4f808c8d9c9417b17381b59f1a2b1f9140a6
Shane Oh <ohshane71@gmail.com>
2021-09-10 09:44:30 +0900

    bug fix on event handling

--- snake_game.py
+++ snake_game.py
@@ -7,12 +7,14 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
+pygame.display.set_caption('Snake game')
+
 SPEED = 5
 BLOCK_SIZE = 20
 
-position = namedtuple('x', 'y')
-position.x = 320
-position.y = 240
+p = namedtuple('x', 'y')
+p.x = 320
+p.y = 240
 
 move_key = None
 
@@ -22,21 +24,21 @@ while True:
             pygame.quit()
             sys.exit()
 
-    if event.type == pygame.KEYDOWN:
-        if event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
-            move_key = event.key
+        if event.type == pygame.KEYDOWN:
+            if event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
+                move_key = event.key
 
     if move_key == pygame.K_UP:
-        position.y -= BLOCK_SIZE
+        p.y -= BLOCK_SIZE
     elif move_key == pygame.K_DOWN:
-        position.y += BLOCK_SIZE
+        p.y += BLOCK_SIZE
     elif move_key == pygame.K_LEFT:
-        position.x -= BLOCK_SIZE
+        p.x -= BLOCK_SIZE
     elif move_key == pygame.K_RIGHT:
-        position.x += BLOCK_SIZE
+        p.x += BLOCK_SIZE
 
     screen.fill((255, 255, 255))
-    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(position.x, position.y, BLOCK_SIZE, BLOCK_SIZE))
+    pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(p.x, p.y, BLOCK_SIZE, BLOCK_SIZE))
 
     pygame.display.update()
 
